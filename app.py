113d30ef7d986b764ab955eaf34dba6219f90d6f
Roberto Marzio <103176073+RobZt12@users.noreply.github.com>
2025-01-21 17:35:52 +0000

    Marzio4

--- app.py
+++ app.py
@@ -4,38 +4,35 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 
-app.config['MYSQL_HOST'] = "138.41.20.102"
-app.config['MYSQL_PORT'] = 53306  
-app.config['MYSQL_USER'] = "ospite"
-app.config['MYSQL_PASSWORD'] = "ospite"
-app.config['MYSQL_DB'] = "w3schools"
+app.config['MYSQL_HOST'] = '138.41.20.102'
+app.config['MYSQL_PORT'] = 53306
+app.config['MYSQL_DB'] = 'w3schools'
+app.config['MYSQL_USER'] = 'ospite'
+app.config['MYSQL_PASSWORD'] = 'ospite'
+
 mysql = MySQL(app)
+
+
+
 @app.route('/')
 def homepage():
-    return render_template('homepage.html',titolo ="home")
-
+    return render_template('homepage.html',titolo = "Home")
 @app.route('/products')
-def products():
+def prodotti():
     cursor = mysql.connection.cursor()
-    query = "SELECT * FROM products"
+    query = 'SELECT * FROM products'
     cursor.execute(query)
-    miao = cursor.fetchall()
+    prodotti = cursor.fetchall()
     cursor.close()
-   
-    return render_template('products.html',titolo = "products",miao=miao)
-
-@app.route('/categories/<int:catID>')
-def categories(catID):
+    return render_template('products.html',titolo = "Prodotti",prodotti=prodotti)
+@app.route('/categories/<CatId>')
+def categories(CatId):
     cursor = mysql.connection.cursor()
-    query = "SELECT * FROM categories"
-    cursor.execute(query)
-    miao = cursor.fetchall()
-    miao2 = []
-    for i in miao:
-        if int(i[0]) == catID:
-            miao2.append(i)
+    query = 'SELECT * FROM categories WHERE CategoryID = %s'
+    cursor.execute(query,(CatId,))
+    categorie = cursor.fetchall()
     cursor.close()
-    return render_template('categories.html',titolo ="categories",catID=catID,miao2=miao2)
+    return render_template('categories.html',titolo = 'Categorie',categorie=categorie,CatId=CatId)
 
 
 if __name__ == '__main__':
